19328db954c8f31bc0c01b699f61697d5ae2845d
in-droid <61251589+in-droid@users.noreply.github.com>
2021-01-05 12:39:59 +0100

    Update Levels.js

--- Seminar/game/Levels.js
+++ Seminar/game/Levels.js
@@ -2,7 +2,7 @@ const LEVELS = {
   0: {
       END_TIME: 45,
       FINISH_COORD : [23.7616, 26.409467, -1.840168],
-      SCENE : '../../common/models/scene1/scene1.gltf',
+      SCENE : '../common/models/scene1/scene1.gltf',
       GENERATOR : [-15, -15, 0],
       GENERATOR_FIXES : 10,
       WALL_WIDTH : 1.9,
@@ -10,7 +10,7 @@ const LEVELS = {
   1 : {
       END_TIME : 20,
       FINISH_COORD : [23.7616, 26.409467, -1.840168],
-      SCENE : '../../common/models/scene1/scene1.gltf',
+      SCENE : '../common/models/scene1/scene1.gltf',
       GENERATOR : [-15, -15, 0],
       GENERATOR_FIXES : 10,
       WALL_WIDTH : 1.9,
@@ -18,7 +18,7 @@ const LEVELS = {
   2 : {
       END_TIME : 15,
       FINISH_COORD : [23.7616, 26.409467, -1.840168],
-      SCENE : '../../common/models/scene1/scene1.gltf',
+      SCENE : '../common/models/scene1/scene1.gltf',
       GENERATOR : [-15, -15, 0],
       GENERATOR_FIXES : 10,
       WALL_WIDTH : 1.9,
